91fe3d98cca461b1b8c34d24fc0a101f7152e4f9
Cokke <JorgeRandomGamer@gmail.com>
2024-07-16 18:53:43 -0400

    register free for all

--- src/main/java/cokke/alura/challenge/forohub/infra/security/SecurityConfigurations.java
+++ src/main/java/cokke/alura/challenge/forohub/infra/security/SecurityConfigurations.java
@@ -27,6 +27,7 @@ public class SecurityConfigurations {
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/usuarios").permitAll()
                         .requestMatchers("/swagger-ui.html", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
                         .anyRequest().authenticated()
                 )
